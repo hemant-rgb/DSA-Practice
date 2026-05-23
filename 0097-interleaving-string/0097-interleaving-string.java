aea28b4f4ace03c6542dcfd05e3bfa90ab95ba7a
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-05-23 13:21:12 +0530

    Time: 5 ms (39.21%), Space: 43.3 MB (55.59%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int n3= s3.length();
+        if(n1+n2!=n3){
+            return false;
+        }
+        boolean [][] dp = new boolean[n1+1][n2+1];
+
+        dp[0][0]=true;
+
+        for(int i=0;i<=n1;i++){
+
+            for(int j=0;j<=n2;j++){
+
+                if(i>0){
+                    dp[i][j]= dp[i][j] || (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+                }
+                if(j>0){
+                    dp[i][j]= dp[i][j] || (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+                }
+
+            }
+        }
+
+        return dp[n1][n2];
+        
+    }
+}
