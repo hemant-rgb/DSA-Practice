c4ecc6e8abee32ad249ba8c729f8a804119fee8f
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-06-01 14:59:03 +0530

    Time: 1 ms (99.93%), Space: 94.6 MB (14.57%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice=Integer.MAX_VALUE;
+        int maxprice=0;
+        for(int price:prices){
+            if(price<minprice){
+                minprice=price;
+            }
+            else{
+                maxprice=Math.max(maxprice,price-minprice);
+            }
+        }
+        return maxprice;
+        
+    }
+}
