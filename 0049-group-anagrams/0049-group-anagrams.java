fad9cb42552dcf25e8d0926ea31ff48a3d6f0223
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-06-12 15:51:19 +0530

    Time: 21 ms (10.6%), Space: 50.2 MB (20.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s : strs){
+            int [] freq= new int[26];
+            for(char c : s.toCharArray()){
+                freq[c-'a']++;
+            }
+            StringBuilder keyBuilder = new StringBuilder();
+            for(int i=0;i<26;i++){
+                keyBuilder.append(freq[i]).append("#");
+            }
+            String key = keyBuilder.toString();
+
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+        
+    }
+}
