6e66284a61547aa12ef7c604fa1b4e311c1b9bc8
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-06-12 15:23:07 +0530

    Time: 1 ms (99.92%), Space: 45.2 MB (79.36%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n= chars.length;
+        int write =0;
+        int i=0;
+        while(i<n){
+            char current = chars[i];
+            int count=0;
+            while(i<n && chars[i]==current){
+                i++;
+                count++;
+            }
+            chars[write++]=current;
+            if(count>1){
+                 String freq = String.valueOf(count);
+                 for(char ch : freq.toCharArray()){
+                    chars[write++]=ch;
+                 }
+            } 
+           
+
+        }
+        return write;
+    }
+}
