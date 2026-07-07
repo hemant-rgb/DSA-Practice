dc03b3452a1a391cebbb6e5f752c1442952d47c5
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-07 10:26:58 +0530

    Time: 0 ms (100%), Space: 43.8 MB (45.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,22 +4,15 @@ class Solution {
         int j = n-1;
         int k = m+n-1;
         while(i>=0 && j>=0){
-            if(nums1[i]>=nums2[j]){
-                nums1[k]= nums1[i];
-                i--;
-
+            if(nums1[i]>nums2[j]){
+                nums1[k--]= nums1[i--];
             }else{
-                nums1[k]=nums2[j];
-                j--;
+                nums1[k--]=nums2[j--];
             }
-
-            k--;
         }
 
         while(j>=0){
-            nums1[k]= nums2[j];
-            j--;
-            k--;
+            nums1[k--]=nums2[j--];
         }
         
     }
