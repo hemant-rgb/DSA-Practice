de42fde115cf68ef534b3a1a29d5f025cc086849
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-08 09:44:54 +0530

    Time: 1 ms (62.58%), Space: 43.3 MB (32.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,15 +1,55 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        for(int i =0;i<strs[0].length();i++){
-            char ch = strs[0].charAt(i);
-            for(int j =1;j<strs.length;j++){
-                if(i==strs[j].length()|| strs[j].charAt(i)!=ch){
-                    return strs[0].substring(0,i);
-                }
+    class Node{
+        Node[] children = new Node[26];
+        boolean eow;
+        
+    }
+    Node root = new Node();
+    private void insert(String word){
+        Node curr = root;
+        for(char ch : word.toCharArray()){
+            int idx = ch-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]= new Node();
             }
+            curr = curr.children[idx];
         }
 
-        return strs[0];
+        curr.eow = true;
+
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        for(String word : strs){
+            insert(word);
+        }
+
+        Node curr = root;
+        int count =0;
+
+        String word = strs[0];
+        StringBuilder sb = new StringBuilder();
+        for(char ch : word.toCharArray()){
+            if(curr.eow){
+                break;
+            }
+            count=0;
+            for(int j=0;j<26;j++){
+                if(curr.children[j]!=null){
+                    count++;
+                }
+                
+            }
+            if(count>1){
+                break;
+            }
+            int idx = ch -'a';
+            sb.append(ch);
+            curr = curr.children[idx];
+
+        }
+
+        return sb.toString();
         
     }
 }
