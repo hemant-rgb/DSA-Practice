c0c20544aeef3050016b02a9f4f1981210cfde21
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-09 09:52:54 +0530

    Time: 6 ms (14.94%), Space: 46.4 MB (17.71%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,40 +1,47 @@
 class Solution {
-
-    private boolean dfs(int src, int dest, ArrayList<ArrayList<Integer>> graph,boolean[]visited){
-        if(src==dest){
-            return true;
-        }
-        visited[src]= true;
-
-        for(int neighbour : graph.get(src)){
-            if(!visited[neighbour]){
-                if(dfs(neighbour,dest,graph,visited)){
-                    return true;
+    private boolean bfs(int src , int end , ArrayList<ArrayList<Integer>> graph,int n){
+        Queue<Integer> q = new LinkedList<>();
+        boolean [] visited = new boolean[n+1];
+        q.offer(src);
+        visited[src]=true;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(curr==end){
+                return true;
+            }
+            for(int neighbour : graph.get(curr)){
+                if(!visited[neighbour]){
+                    visited[neighbour]=true;
+                    q.offer(neighbour);
                 }
             }
         }
 
         return false;
     }
+
+
     public int[] findRedundantConnection(int[][] edges) {
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<=edges.length;i++){
+        int n = edges.length;
+
+        for(int i=0;i<=n;i++){
             graph.add(new ArrayList<>());
         }
-        
-        
-        for(int []edge: edges){
+      
+        for(int []edge : edges){
             int u = edge[0];
             int v = edge[1];
-            boolean[] visited = new boolean[edges.length+1];
-
-            if(!graph.get(u).isEmpty() && !graph.get(v).isEmpty() && dfs(u,v,graph,visited)){
+            if(!graph.get(u).isEmpty() && !graph.get(v).isEmpty() && bfs(u,v,graph,n)){
                 return edge;
             }
 
             graph.get(u).add(v);
             graph.get(v).add(u);
+            
         }
+        
+
 
         return new int[]{};
     }
