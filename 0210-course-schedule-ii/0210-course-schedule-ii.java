2925b95c2bdc815d53e1b4963c25df1c12b5ec2b
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-10 09:14:39 +0530

    Time: 5 ms (83.37%), Space: 47 MB (30.77%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,49 +1,45 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-       ArrayList< Integer>[] graph = new ArrayList[numCourses];
-       for(int i=0;i<graph.length;i++){
-        graph[i]= new ArrayList<>();
-       }
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int n =prerequisites.length;
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        int[]indegree = new int[numCourses];
+        for(int []edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(v).add(u);
+            indegree[u]++;
 
-       int [] indeg = new int[graph.length];
-
-       for(int []pre : prerequisites){
-        
-        graph[pre[1]].add(pre[0]);
-        indeg[pre[0]]++;
-
-       }
-
-
-       Queue<Integer> q = new LinkedList<>();
-       for(int i=0;i<numCourses;i++){
-        if(indeg[i]==0){
-            q.add(i);
         }
 
-       }
+        Queue<Integer> q = new LinkedList<>();
+        int[]ans = new int[numCourses];
+        int index =0;
 
-       int ans[] = new int[numCourses];
-       int idx=0;
-       while(!q.isEmpty()){
-        int curr = q.remove();
-        ans[idx++]=curr;
-        for(int i=0;i<graph[curr].size();i++){
-            int dest = graph[curr].get(i);
-            indeg[dest]--;
-            if(indeg[dest]==0){
-                q.add(dest);
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.offer(i);
             }
         }
 
-       }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            ans[index++]= curr;
 
-       if(idx != numCourses){
-           return new int[0];
-       }
+            for(int neighbour: graph.get(curr)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.offer(neighbour);
+                }
+            }
+        }
 
-       return ans;
-       
+        return index==numCourses ? ans : new int[]{};
+
+
+    
         
     }
 }
