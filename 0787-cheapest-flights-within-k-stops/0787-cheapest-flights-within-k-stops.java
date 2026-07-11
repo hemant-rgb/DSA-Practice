f69faec49aea7cf4c4869e9902d6217df8eced01
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-11 10:29:55 +0530

    Time: 6 ms (85.7%), Space: 46.6 MB (40.11%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,22 +1,50 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int [] dist = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src]=0;
-        for(int i=0;i<=k;i++){
-            int []temp = dist.clone();
-            for(int [] flight : flights){
-                int u = flight[0];
-                int v = flight[1];
-                int wt = flight[2];
+        ArrayList<int[]>[] graph = new ArrayList[n];
 
-                if(dist[u]!= Integer.MAX_VALUE && dist[u] + wt <temp[v]){
-                    temp[v]=dist[u]+wt;
-                }
-            }
-            dist=temp;
+        for(int i=0;i<n ;i++){
+            graph[i] = new ArrayList<>();
         }
 
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        for(int[] f: flights){
+            graph[f[0]].add(new int[]{f[1],f[2]});
+        }
+
+        Queue<int[]> queue =  new LinkedList<>();
+
+        int[]dist = new int[n];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dist[src]=0;
+        queue.offer(new int[]{src,0,0});
+
+        while(!queue.isEmpty()){
+            int[]curr = queue.poll();
+            int city = curr[0];
+            int cost =curr[1];
+            int stop = curr[2];
+            if(stop>k){
+                continue;
+            }
+
+            for(int[] neighbour : graph[city]){
+                int next = neighbour[0];
+                int price = neighbour[1];
+
+                if(cost + price < dist[next]){
+                    dist[next]= cost + price;
+
+                    queue.offer(new int[]{
+                        next,
+                        cost + price,
+                        stop+1
+                    });
+                }
+            }
+        } 
+
+
+        return dist[dst] ==Integer.MAX_VALUE ? -1: dist[dst];       
     }
 }
