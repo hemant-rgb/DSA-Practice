39e59e338dc2c0de4080cc729108ca84541fb3f1
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-14 11:37:19 +0530

    Time: 25 ms (79.43%), Space: 44 MB (94.82%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,24 +19,29 @@ class Node {
 */
 
 class Solution {
-    HashMap<Node,Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-
         if(node == null){
             return null;
         }
-        if(map.containsKey(node)){
-            return map.get(node);
+
+        HashMap<Node,Node> map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        
+        map.put(node,new Node(node.val));
+        q.offer(node);
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            for(Node neighbor : curr.neighbors){
+                if(!map.containsKey(neighbor)){
+                    map.put(neighbor, new Node(neighbor.val));
+                    q.offer(neighbor);
+                }
+
+                map.get(curr).neighbors.add(map.get(neighbor));
+            }
         }
-        Node clone = new Node(node.val);
-        map.put(node,clone);
 
-        for(Node neighbor : node.neighbors){
-            clone.neighbors.add(cloneGraph(neighbor));
-        }
-
-        return clone;
-
+        return map.get(node);
         
     }
 }
