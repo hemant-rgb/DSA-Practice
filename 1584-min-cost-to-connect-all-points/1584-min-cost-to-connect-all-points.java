461d743c239bf8ba8f471e7b1e13e29871b37625
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-07-14 11:04:16 +0530

    Time: 76 ms (69.62%), Space: 77.1 MB (51.17%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,77 +1,38 @@
 class Solution {
-
-
-    class DSU{
-        int[]parent;
-        int []rank;
-
-        DSU(int n){
-            parent = new int[n];
-            rank = new int[n];
-            for(int i=0;i<n;i++){
-                parent[i]=i;
-            }
-        }
-
-        int find(int x){
-            if(parent[x]!=x){
-                parent[x]=find(parent[x]);
-            }
-
-            return parent[x];
-
-        }
-
-        boolean union(int x, int y){
-            int px = find(x);
-            int py = find(y);
-            if(px == py){
-                return false;
-            }
-
-            if(rank[px]==rank[py]){
-                parent[px]=py;
-                rank[py]++;
-            }else if(rank[px]<rank[py]){
-                parent[px]=py;
-                
-            }else{
-                parent[py]=px;
-                
-            }
-
-            return true;
-        }
-    }
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        ArrayList<int[]> edges = new ArrayList<>();
 
-        for(int i=0;i<n ;i++){
-            for(int j=i+1;j<n;j++){
-                int cost = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+        boolean[]visited = new boolean[n];
+        PriorityQueue<int[]> pq =new PriorityQueue<>(
+            (a,b)-> a[0]-b[0]
+        );
+        pq.offer(new int[]{0,0});
+        int ans=0;
+        int nodeCount=0;
+        while(nodeCount<n){
+            int[] curr = pq.poll();
+            int cost =curr[0];
+            int node = curr[1];
 
-                edges.add(new int[]{i,j,cost});
+            if(visited[node]==true){
+                continue;
             }
-        }
-        edges.sort((a,b)-> a[2]-b[2]);
+            visited[node]=true;
+            ans+=cost;
+            nodeCount++;
 
-        DSU dsu = new DSU(n);
-        int answer =0;
-        int used=0;
-
-        for(int []edge : edges){
-            if(dsu.union(edge[0],edge[1])){
-                answer+=edge[2];
-                used++;
-                if(used ==n-1){
-                    break;
+            for(int i=0;i<n;i++){
+                if(!visited[i]){
+                    int dist = Math.abs(points[node][0]-points[i][0]) + Math.abs(points[node][1]-points[i][1]);
+                    pq.offer(new int[]{
+                        dist,i
+                    });
                 }
             }
         }
 
-        return answer;
 
+        return ans;
         
     }
 }
