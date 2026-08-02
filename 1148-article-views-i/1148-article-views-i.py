667e7610df3fca66974cca8cb7057c781f86e18b
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-08-02 14:10:09 +0530

    Time: 269 ms (87.83%), Space: 67.2 MB (72.87%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    article_views_df = views[(views['author_id']==views['viewer_id'])]
+    article_views_df = article_views_df[['author_id']].drop_duplicates()
+    article_views_df = article_views_df.sort_values(by = 'author_id')
+
+    return article_views_df[['author_id']].rename(columns = {'author_id': 'id'})
+    
