7a44e359b6fe137b432b558c96956ed82a9bf3c4
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-08-08 14:34:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.58%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int solve(int n , int[]dp){
+        if(n==0 || n == 1) return dp[n]=1;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+    }
+    public int climbStairs(int n) {
+        int []dp = new int [n+1];
+
+        Arrays.fill(dp,-1);
+
+        solve(n,dp);
+        return dp[n];
+        
+    }
+}
