0ec8dc81ca6290dbe6986dcf12d1e9c236a66e39
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-08-08 15:45:52 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -13,6 +13,7 @@ class Solution {
     }
     public int rob(int[] nums) {
         int n= nums.length;
+        if(n==1)return nums[0];
         int[]dp = new int[n];
         Arrays.fill(dp,-1);
 
