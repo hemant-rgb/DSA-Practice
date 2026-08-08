f9835c4704759309a67933bb3b078fd1b62338d7
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-08-08 16:05:19 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -25,28 +25,49 @@ class Solution {
     // }
 
     public int rob(int []nums){
+        // int n = nums.length;
+        // if(n==0)return 0;
+        // if(n==1)return nums[0];
+        // if(n==2) return Math.max(nums[0],nums[1]);
+
+        // int[]dp = new int[n];
+        // dp[0]= nums[0];
+        
+        // for(int i=1;i<n;i++){
+        //     int left = nums[i];
+        //     if(i>1){
+        //         left += dp[i-2];
+
+        //     }
+            
+        //     int right = dp[i-1];
+
+        //     dp[i]= Math.max(left,right);
+
+        // }
+
+        // return dp[n-1];
+
         int n = nums.length;
         if(n==0)return 0;
-        if(n==1)return nums[0];
+        if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
 
-        int[]dp = new int[n];
-        dp[0]= nums[0];
-        
+        int prev1 = nums[0];
+        int prev2 = 0;
         for(int i=1;i<n;i++){
-            int left = nums[i];
-            if(i>1){
-                left += dp[i-2];
-
-            }
+            int left =prev2 + nums[i];
             
-            int right = dp[i-1];
 
-            dp[i]= Math.max(left,right);
+            int right = prev1;
+            int curr = Math.max(left,right);
 
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n-1];
+        return prev1;
+
         
     }
 }
