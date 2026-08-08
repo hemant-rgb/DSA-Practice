dc82c6ca0f5b5a1813bdbca5723d6408424ed809
Hemant Machiwar <hemantmachiwar76@gmail.com>
2026-08-08 15:45:36 +0530

    Time: 0 ms (100%), Space: 43 MB (19.35%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,25 @@
+
+class Solution {
+
+    private int solve(int n, int[]dp,int[]nums){
+        if(n==0) return nums[0];
+        if(n==-1) return 0;
+
+        if(dp[n]!=-1)return dp[n];
+
+        int left = nums[n] + solve(n-2,dp,nums);
+        int right = solve(n-1,dp,nums);
+        return dp[n]= Math.max(left,right);
+    }
+    public int rob(int[] nums) {
+        int n= nums.length;
+        int[]dp = new int[n];
+        Arrays.fill(dp,-1);
+
+        dp[0]=nums[0];
+        solve(n-1,dp,nums);
+
+        return dp[n-1];
+
+    }
+}
